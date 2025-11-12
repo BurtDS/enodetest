4b83e7c773bb6f9c59f5ca7fd61076e104d9611d
BurtDS <bert@vulpo.be>
2025-11-12 20:18:46 +0100

    add scheduler

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,20 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Schedule vehicle data synchronization
+Schedule::command('vehicles:sync')
+    ->hourly()
+    ->withoutOverlapping()
+    ->onOneServer();
+
+// Schedule vehicle data refresh
+Schedule::command('vehicles:refresh --all')
+    ->everyThirtyMinutes()
+    ->withoutOverlapping()
+    ->onOneServer();
